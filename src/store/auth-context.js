9002f01acd9a6f526dcb602ed4d77b3073080761
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-05-12 16:02:53 +0530

    React-Context-&-useContext-Hook--task-21

---
+++ src/store/auth-context.js
@@ -0,0 +1,9 @@
+// Write your code here:
+import React from "react";
+const AuthContext = React.createContext(
+  {
+    isLoggedIn:false,
+    onLogout:()=>{},
+   }
+);
+export default AuthContext;
